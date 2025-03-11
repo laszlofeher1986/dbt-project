dfcbe8c84b60657331b0ddb829cc1cf9f6bef0c8
László Fehér <laszlo.feher@dezerius.com>
2025-03-11 13:45:59 +0100

    comment 2

--- target/compiled/my_personal_project/models/gold/g_exchange_rates.sql
+++ target/compiled/my_personal_project/models/gold/g_exchange_rates.sql
@@ -1,1 +1,2 @@
+/* load the silver layer*/
 SELECT * FROM "WH_LF"."silver"."s_exchange_rates"
--- target/run/my_personal_project/models/gold/g_exchange_rates.sql
+++ target/run/my_personal_project/models/gold/g_exchange_rates.sql
@@ -4,7 +4,8 @@
     
     
 
-    EXEC('CREATE TABLE "WH_LF"."gold"."g_exchange_rates__dbt_temp" AS SELECT * FROM "WH_LF"."silver"."s_exchange_rates" 
+    EXEC('CREATE TABLE "WH_LF"."gold"."g_exchange_rates__dbt_temp" AS /* load the silver layer*/
+SELECT * FROM "WH_LF"."silver"."s_exchange_rates" 
     OPTION (LABEL = ''dbt-fabric-dw'');
 ');
 
